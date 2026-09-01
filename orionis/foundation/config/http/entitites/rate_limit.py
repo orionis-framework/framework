11a55167f2b6101b9bc350bdedc6502ae375fb55
rmunate <raulmauriciounate@gmail.com>
2026-09-01 12:25:44 -0500

    ✨ [rate_limit] - Update

--- orionis/foundation/config/http/entitites/rate_limit.py
+++ orionis/foundation/config/http/entitites/rate_limit.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from orionis.environment.env import Env
+from orionis.environment.facade import Env
 from orionis.support.entities.base import BaseEntity
 
 @dataclass(frozen=True, kw_only=True)

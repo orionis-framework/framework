a6e7645edda2ca59f56e92da6cd6c8d643d64461
rmunate <raulmauriciounate@gmail.com>
2026-09-01 12:25:40 -0500

    🔧 [redis] - Update

--- orionis/foundation/config/cache/entities/redis.py
+++ orionis/foundation/config/cache/entities/redis.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from orionis.environment.env import Env
+from orionis.environment.facade import Env
 from orionis.support.entities.base import BaseEntity
 
 # Highest valid TCP/UDP port number.

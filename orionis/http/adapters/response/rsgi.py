4357c103cead24a7a3fc3b05e4324db56726d524
rmunate <raulmauriciounate@gmail.com>
2026-09-01 15:17:51 -0500

    🔥 [rsgi] - Update

--- orionis/http/adapters/response/rsgi.py
+++ orionis/http/adapters/response/rsgi.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 from orionis.http.adapters.response.contracts.response import ResponseAdapter
-from orionis.http.response import FileResponse, Response
+from orionis.http.responses import FileResponse, Response
 
 if TYPE_CHECKING:
     from granian.rsgi import HTTPProtocol

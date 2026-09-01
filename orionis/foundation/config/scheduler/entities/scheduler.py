aab9194abcf13f2f9593e7f6b1c142254f272598
rmunate <raulmauriciounate@gmail.com>
2026-09-01 12:25:45 -0500

    🛠️ [scheduler] - Update

--- orionis/foundation/config/scheduler/entities/scheduler.py
+++ orionis/foundation/config/scheduler/entities/scheduler.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from orionis.environment.env import Env
+from orionis.environment.facade import Env
 from orionis.foundation.config.scheduler.entities.stores import Stores
 from orionis.foundation.config.scheduler.enums.drivers import Drivers
 from orionis.support.entities.base import BaseEntity

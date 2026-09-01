7c60655784e34b9c89ba64409d7b0f977d7e02d4
rmunate <raulmauriciounate@gmail.com>
2026-09-01 12:25:45 -0500

    🌟 [session] - Update

--- orionis/foundation/config/session/entities/session.py
+++ orionis/foundation/config/session/entities/session.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from orionis.foundation.config.session.enums import SameSitePolicy
 from orionis.foundation.config.session.enums.drivers import SessionDriver
-from orionis.environment.env import Env
+from orionis.environment.facade import Env
 from orionis.support.entities.base import BaseEntity
 
 # Pre-computed frozensets enable O(1) membership tests at validation time.

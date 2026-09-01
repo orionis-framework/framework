7f251b0101f46a3ccc7f337871a8f977653ccbfc
rmunate <raulmauriciounate@gmail.com>
2026-09-01 12:25:44 -0500

    💡 [logging] - Update

--- orionis/foundation/config/logging/entities/logging.py
+++ orionis/foundation/config/logging/entities/logging.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass, field, fields
 from orionis.foundation.config.logging.entities.channels import Channels
-from orionis.environment.env import Env
+from orionis.environment.facade import Env
 from orionis.support.entities.base import BaseEntity
 
 # Pre-computed channel option names

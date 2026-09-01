bdfe73ef3840926da040a4eb0efb8a5284e0b5bc
rmunate <raulmauriciounate@gmail.com>
2026-09-01 15:17:56 -0500

    🏗️ [types] - Update

--- orionis/http/types.py
+++ orionis/http/types.py
@@ -1,4 +1,4 @@
-from orionis.http.response import Response
+from orionis.http.responses import Response
 
 # Every object built by the ``response`` factory derives from ``Response``,
 # so this single alias annotates any handler regardless of its content type.

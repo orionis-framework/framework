953706ddb06ff2929bb3965abf10419dc7b03ee4
rmunate <raulmauriciounate@gmail.com>
2026-09-14 13:02:58 -0500

    🔥 [web] - Update

--- routes/web.py
+++ routes/web.py
@@ -1,13 +1,29 @@
 from app.http.controllers.auth.login_controller import LoginController
 from app.http.controllers.auth.register_controller import RegisterController
-from app.http.controllers.auth.forgot_password_controller import ForgotPasswordController
+from app.http.controllers.auth.forgot_password_controller import (
+    ForgotPasswordController,
+)
 from app.http.controllers.home_controller import HomeController
+from orionis.auth.middleware import (
+    AuthenticateSessionMiddleware,
+    GuestMiddleware,
+    ResolveIdentityMiddleware,
+)
 from orionis.support.facades.router import Route
 
-Route.get("/", [HomeController, "index"])
-Route.get("/login", [LoginController, "index"])
-Route.post("/login", [LoginController, "login"]).name("login")
-Route.get("/sign-up", [RegisterController, "index"])
-Route.post("/sign-up", [RegisterController, "register"]).name("register")
+Route.get("/", [HomeController, "index"]).middleware(ResolveIdentityMiddleware)
+Route.get("/login", [LoginController, "index"]).middleware(GuestMiddleware)
+Route.post("/login", [LoginController, "login"]).name("login").middleware(
+    GuestMiddleware,
+)
+Route.get("/sign-up", [RegisterController, "index"]).middleware(GuestMiddleware)
+Route.post("/sign-up", [RegisterController, "register"]).name("register").middleware(
+    GuestMiddleware,
+)
+Route.post("/logout", [LoginController, "logout"]).name("logout").middleware(
+    AuthenticateSessionMiddleware,
+)
 Route.get("/forgot-password", [ForgotPasswordController, "index"])
-Route.post("/forgot-password", [ForgotPasswordController, "sendResetLink"]).name("forgot-password")
+Route.post("/forgot-password", [ForgotPasswordController, "sendResetLink"]).name(
+    "forgot-password",
+)

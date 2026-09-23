323b7c8577a98a61a238ccc0360605820d883471
rmunate <raulmauriciounate@gmail.com>
2026-09-22 22:54:04 -0500

    🚀 [types] - Update

--- orionis/http/routes/types.py
+++ orionis/http/routes/types.py
@@ -1,8 +1,9 @@
 from collections.abc import Callable, Sequence
 from collections.abc import Set as AbstractSet
-
 from orionis.http.middleware import BaseMiddleware
 
+# Callable represents Python functions here; parse_action rejects callable
+# instances, bound methods, builtins and lambdas during registration.
 type RouteAction = Callable | type | list[type | str] | tuple[type, str]
 type MiddlewareInput = (
     type[BaseMiddleware]

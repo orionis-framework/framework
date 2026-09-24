f7e3e82cb60daf71d39cf27e338ce4dc085e2d4b
rmunate <raulmauriciounate@gmail.com>
2026-09-23 19:44:50 -0500

    🛠️ [__init__] - Update

--- orionis/auth/concerns/__init__.py
+++ orionis/auth/concerns/__init__.py
@@ -1,9 +1,11 @@
 from orionis.auth.concerns.authenticatable import Authenticatable
 from orionis.auth.concerns.authorizable import Authorizable
 from orionis.auth.concerns.functions import model_primary_key
+from orionis.auth.concerns.must_verify_email import MustVerifyEmail
 
 __all__ = [
     "Authenticatable",
     "Authorizable",
+    "MustVerifyEmail",
     "model_primary_key",
 ]

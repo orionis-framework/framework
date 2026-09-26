41e1fcad8505b8763bc15bb0bfb8cd67d8a4895e
rmunate <raulmauriciounate@gmail.com>
2026-09-25 21:57:56 -0500

    🎨 [api] - Update

--- routes/api.py
+++
@@ -1,6 +0,0 @@
-from app.http.controllers.user_controller import UserController
-from orionis.support.facades.router import Route
-
-Route.group(prefix="api/v1", routes=[
-    Route.get("/{slug:str}/{identifier:int}", [UserController, "index"]),
-])

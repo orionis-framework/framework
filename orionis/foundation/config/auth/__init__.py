6b8a2abfe1db0c35d2e2aedc755335adbf6ca5ec
rmunate <raulmauriciounate@gmail.com>
2026-09-25 21:57:16 -0500

    🚀 [__init__] - Update

--- orionis/foundation/config/auth/__init__.py
+++ orionis/foundation/config/auth/__init__.py
@@ -1,5 +1,7 @@
 from orionis.foundation.config.auth.entities.auth import Auth
 from orionis.foundation.config.auth.entities.identity import Identity
+from orionis.foundation.config.auth.entities.password_reset import PasswordReset
+from orionis.foundation.config.auth.entities.remember import RememberAuth
 from orionis.foundation.config.auth.entities.session import SessionAuth
 from orionis.foundation.config.auth.entities.tokens import Tokens
 from orionis.foundation.config.auth.enums.guards import Guards
@@ -8,6 +10,8 @@ __all__ = [
     "Auth",
     "Guards",
     "Identity",
+    "PasswordReset",
+    "RememberAuth",
     "SessionAuth",
     "Tokens",
 ]

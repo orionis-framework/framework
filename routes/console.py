101c831d62f3af019450a6074f58e81737bfa6b5
rmunate <raulmauriciounate@gmail.com>
2026-09-25 21:57:56 -0500

    💡 [console] - Update

--- routes/console.py
+++ routes/console.py
@@ -1,15 +1,14 @@
-from app.services.welcome_service import WelcomeService
 from orionis.console.args.argument import Argument
 from orionis.support.facades.reactor import Reactor
 
 # Example console command definition
-Reactor.command("app:test", [WelcomeService, "greetUser"])\
-       .timestamp()\
-       .description("Command Test Defined as Route")\
-       .arguments([
-            Argument(
-                name_or_flags=["--name", "-n"],
-                type_=str,
-                required=False,
-            ),
-       ])
+# Reactor.command("app:test", [WelcomeService, "greetUser"])\
+#        .timestamp()\
+#        .description("Command Test Defined as Route")\
+#        .arguments([
+#             Argument(
+#                 name_or_flags=["--name", "-n"],
+#                 type_=str,
+#                 required=False,
+#             ),
+#        ])

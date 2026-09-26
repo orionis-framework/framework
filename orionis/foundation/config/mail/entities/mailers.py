db625cb298b28d101ab1e2d3ff3714be12fabeb2
rmunate <raulmauriciounate@gmail.com>
2026-09-25 21:57:44 -0500

    🧹 [mailers] - Update

--- orionis/foundation/config/mail/entities/mailers.py
+++ orionis/foundation/config/mail/entities/mailers.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
+
 from dataclasses import dataclass, field
+
 from orionis.foundation.config.mail.entities.file import File
 from orionis.foundation.config.mail.entities.smtp import Smtp
 from orionis.support.entities.base import BaseEntity
+
 
 @dataclass(frozen=True, kw_only=True)
 class Mailers(BaseEntity):
@@ -53,19 +56,16 @@ class Mailers(BaseEntity):
             This method does not return a value.
         """
         # Validate `smtp` attribute type and convert dict to Smtp if needed
+        super().__post_init__()
         if not isinstance(self.smtp, (Smtp, dict)):
-            error_msg = (
-                "The 'smtp' attribute must be a Smtp object or a dictionary."
-            )
+            error_msg = "The 'smtp' attribute must be a Smtp object or a dictionary."
             raise TypeError(error_msg)
         if isinstance(self.smtp, dict):
             object.__setattr__(self, "smtp", Smtp(**self.smtp))
 
         # Validate `file` attribute type and convert dict to File if needed
         if not isinstance(self.file, (File, dict)):
-            error_msg = (
-                "The 'file' attribute must be a File object or a dictionary."
-            )
+            error_msg = "The 'file' attribute must be a File object or a dictionary."
             raise TypeError(error_msg)
         if isinstance(self.file, dict):
             object.__setattr__(self, "file", File(**self.file))

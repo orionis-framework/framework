3af5c1e64bac634944ac4ffe80cb632b3281c203
rmunate <raulmauriciounate@gmail.com>
2026-09-25 21:57:33 -0500

    🚀 [disk_name] - Update

--- orionis/foundation/config/filesystems/enums/disk_name.py
+++ orionis/foundation/config/filesystems/enums/disk_name.py
@@ -23,8 +23,8 @@ class DiskName(StrEnum):
         An enumeration member representing a filesystem disk name.
     """
 
-    LOCAL = "local"      # Local private disk
-    PUBLIC = "public"    # Local public disk
-    S3 = "s3"            # AWS S3 disk
-    AZURE = "azure"      # Azure Blob Storage disk
-    GCS = "gcs"          # Google Cloud Storage disk
+    LOCAL = "local"  # Local private disk
+    PUBLIC = "public"  # Local public disk
+    S3 = "s3"  # AWS S3 disk
+    AZURE = "azure"  # Azure Blob Storage disk
+    GCS = "gcs"  # Google Cloud Storage disk
